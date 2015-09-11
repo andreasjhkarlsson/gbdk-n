295450f77f418e10b065bc0d43c5604f802a8e1c
Andreas Karlsson <andreas.karlsson3@gmail.com>
2015-09-11 13:09:15 +0200

    Add standard data location to link script (fixes old gbdk compatibility)

--- bin/gbdk-n-link.sh
+++ bin/gbdk-n-link.sh
@@ -4,4 +4,4 @@ GBDK_DIR="$( cd "$( dirname "$0" )" && pwd )/.."
 
 set -x
   
-sdcc -mgbz80 --no-std-crt0 -I "$GBDK_DIR/include" -I "$GBDK_DIR/include/asm" -L "$GBDK_DIR/lib" "$GBDK_DIR/lib/crt0.rel" -l gb.lib -o a.ihx "$@"
+sdcc -mgbz80 --no-std-crt0 --data-loc 0xc0a0 -I "$GBDK_DIR/include" -I "$GBDK_DIR/include/asm" -L "$GBDK_DIR/lib" "$GBDK_DIR/lib/crt0.rel" -l gb.lib -o a.ihx "$@"
